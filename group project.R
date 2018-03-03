176ae72d783cb00776d0065c03463ca9d85eec2d
haoli53 <h53@uw.edu>
2018-03-03 15:59:24 -0800

    data

--- group project.R
+++ group project.R
@@ -1,13 +1,6 @@
-library("jsonlite")
 library("dplyr")
 library("knitr")
 library("httr")
 
-source('key script.R') 
-query.params <- list(access_token = apikeys.R)
-apikeys.R
-
-base.uri <- "http://api.data.gov/ed/collegescorecard/"
-resource.uri <- paste0("/v1/schools")
-endpoint.data <- paste0(base.uri, resource.uri)
-response.data <- GET(endpoint.data, add_headers('X-API-Key' = apikeys.R))
+data <- read.csv("Most-Recent-Cohorts-Scorecard-Elements.csv")
+data11 <- read.csv("Most-Recent-Cohorts-Treasury-Elements.csv")
